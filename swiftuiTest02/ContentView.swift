cfae58451e5882ae9434ba097f59fee21f2807fe
jhchoo <jhchoo@jhchooui-MacBookPro.local>
2021-08-02 18:44:41 +0900

    first commit

---
+++ swiftuiTest02/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  swiftuiTest02
+//
+//  Created by jhchoo on 2021/08/02.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ swiftuiTest02/swiftuiTest02App.swift
@@ -0,0 +1,17 @@
+//
+//  swiftuiTest02App.swift
+//  swiftuiTest02
+//
+//  Created by jhchoo on 2021/08/02.
+//
+
+import SwiftUI
+
+@main
+struct swiftuiTest02App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
