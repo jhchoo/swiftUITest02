bcd8afe0daaaf019808d8198a1fe2552559f5650
jhchoo <jhchoo.kr@gmail.com>
2021-08-02 19:02:17 +0900

    1강

---
+++ swiftuiTest02/01.기본가이드/S01BasicGuideView.swift
@@ -0,0 +1,21 @@
+//
+//  S01BasicGuideView.swift
+//  swiftuiTest02
+//
+//  Created by jhchoo on 2021/08/02.
+//
+
+import SwiftUI
+
+struct S01BasicGuideView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct S01BasicGuideView_Previews: PreviewProvider {
+    static var previews: some View {
+        S01BasicGuideView()
+    }
+}
