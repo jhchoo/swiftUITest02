5b3d55a3fbdc6da72543064b2bfad05886a21b14
jhchoo <jhchoo.kr@gmail.com>
2021-08-13 23:36:08 +0900

    123

--- README.md
+++ README.md
@@ -8,7 +8,7 @@
 3. 사용자 입력 처리 [https://developer.apple.com/tutorials/swiftui/handling-user-input]
 4. 경로 및 모양 그리기 [https://developer.apple.com/tutorials/swiftui/drawing-paths-and-shapes]
 5. 보기 및 전환 애니메이션 [https://developer.apple.com/tutorials/swiftui/animating-views-and-transitions]
-
+----
 6. 복잡한 인터페이스 구성 [https://developer.apple.com/tutorials/swiftui/composing-complex-interfaces]
 
 
--- swiftuiTest02/ContentView.swift
+++ swiftuiTest02/ContentView.swift
@@ -12,7 +12,7 @@ struct ContentView: View {
     let myNavigationTitle = "스위프트UI 테스트"
     let listName:[String] = ["01. 애플 기본 샘플",
                              "02. 애플 리스트 샘플",
-                             "03.",
+                             "03. 복잡한 인터페이스 구성",
                              "04.",
                              "05."]
     
@@ -28,6 +28,8 @@ struct ContentView: View {
                             NavigationLink(listName[index], destination: S01AppleLandmarkDetail(landmark: ModelData().landmarks[0]))
                         } else if index == 1 {
                             NavigationLink(listName[index], destination: S01AppleLandmarkList())
+                        } else if index == 2 {
+                            NavigationLink(listName[index], destination: S03CategoryHome())
                         } else {
                            Text(String(listName[index]))
                            .listRowBackground(Color.green)
